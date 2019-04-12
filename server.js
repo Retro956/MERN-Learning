0b52cd2e7d1a958dae8a5ae02a3ee3265f6aa062
Retro956 <Mooreka956@yahoo.com>
2019-04-12 11:14:22 -0400

    Added deeper token functionality to /login and /current

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -17,6 +18,12 @@ const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose.connect(db).then(() => console.log('MongoDB Connected')).catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
 
 // Port to use
 const port = process.env.PORT || 8081;
